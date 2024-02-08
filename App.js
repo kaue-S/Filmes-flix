63f4dc4c2be7d615601599b2998b9595bd440e6e
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-02-08 10:53:33 -0300

    Criação dos estilos

--- App.js
+++ App.js
@@ -1,14 +1,61 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <Text>App</Text>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.viewLogo}>
+          <Text>Filmes Flix</Text>
+        </View>
+        <View style={estilos.viewBotoes}>
+          <Button title="Buscar Filmes" />
+          <Button title="Favoritos" />
+        </View>
+        <View style={estilos.viewRodape}>
+          <Button title="Privacidade" />
+          <Button title="Sobre" />
+        </View>
       </SafeAreaView>
     </>
   );
 }
 
-const estilos = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+
+  viewLogo: {
+    flex: 1,
+    backgroundColor: "green",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+
+  viewBotoes: {
+    flex: 1,
+    justifyContent: "space-around",
+    flexDirection: "row",
+    backgroundColor: "orange",
+    alignItems: "baseline",
+  },
+
+  viewRodape: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "red",
+    flex: 0.2,
+  },
+});

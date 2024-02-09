e933947c4a64307b5673ecba2170bc215c154379
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-02-09 11:59:17 -0300

    comit

--- App.js
+++ App.js
@@ -11,17 +11,40 @@ import {
 import logo from "./assets/images/logotipo.png";
 import { Ionicons } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+/* manter a tela splash visível enquanto não programarmos a ação de ocultar */
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Monoton-Regular": require("./assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  /*Função atrelada ao hook usaCallback. Quando uma função está conectada ao useCallBack, garantimos que a referência dela é armazenada somente uma vez. */
+  const aoAtualizarLayout = useCallback(async () => {
+    /*Se estiver tudo ok com o carregamento */
+    if (fontsLoaded || fontError) {
+      /*Escondemos a splashscreen */
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView style={estilos.container}>
+      <SafeAreaView style={estilos.container} onLayout={aoAtualizarLayout}>
         <View style={estilos.viewLogo}>
           <Image source={logo} style={estilos.logo} />
-          <Text>Filmes Flix</Text>
+          <Text style={estilos.titulo}> Filmes Flix</Text>
         </View>
-        
+
         <View style={estilos.viewBotoes}>
           <Pressable style={estilos.botao}>
             <Text style={estilos.textoBotao}>
@@ -59,6 +82,7 @@ const estilos = StyleSheet.create({
     backgroundColor: "rgb(31,30,27)",
     justifyContent: "flex-end",
     alignItems: "center",
+    paddingBottom: 15,
   },
 
   logo: {
@@ -66,23 +90,30 @@ const estilos = StyleSheet.create({
     height: 250,
   },
 
+  titulo: {
+    color: "white",
+    fontFamily: "Monoton-Regular",
+    fontSize: 36,
+  },
+
   viewBotoes: {
     flex: 1,
     justifyContent: "space-around",
     flexDirection: "row",
-    alignItems: "baseline",
+    alignItems: "center",
   },
 
   botao: {
     borderRadius: 5,
     padding: 16,
     backgroundColor: "gold",
+    alignItems: "center",
   },
 
   textoBotao: {},
 
   viewRodape: {
-    justifyContent: "space-between",
+    justifyContent: "space-around",
     alignItems: "center",
     flexDirection: "row",
     backgroundColor: "red",

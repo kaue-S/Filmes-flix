83b7ee49b2ab9007bd1326771c3e41aa7590225b
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-02-26 09:18:00 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from "react-native";
 import Home from "./src/screens/Home";
-import Sobre from "./src/screens/Sobre";
-import Privacidade from "./src/screens/Privacidade";
 
-/* manter a tela splash visível enquanto não programarmos a ação de ocultar */
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
